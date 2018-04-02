1b7902d412babf8badd6555e9194f0af1953983f
Sherman Gray <shermangray@hotmail.com>
2018-04-02 15:49:46 -0300

    ordenado por nome

--- script/index.php
+++ script/index.php
@@ -6,7 +6,7 @@ $smarty->template_dir = '../template';
 $smarty->compile_dir = '../template_c';
 include('../include/conexao.php');
 if (empty($_GET['nome'])) {
-    $ps=$conexao->prepare("select * from contatos");
+    $ps=$conexao->prepare("select * from contatos order by nome");
     $ps->execute();
 } else {
     $ps=$conexao->prepare("select * from contatos where nome like ?");
