057344acf9fb89de14dcbdd93b8e2a906d3bb750
Sherman Gray <shermangray@hotmail.com>
2018-04-27 16:26:49 -0300

    mais uma correÃÃo para atualizar foto dos contatos

--- script/atualizacontato.php
+++ script/atualizacontato.php
@@ -12,6 +12,6 @@ $filho = $_POST['filho'];
 $ps=$conexao->prepare("update contatos set nome = ?, email = ?, celular = ?, nascimento = ?, filho = ? where id = ?");
 $ps->execute(array($nome, $email, $celular, $nascimento, $filho, $id));
 $info = new SplFileInfo($_FILES['arquivo']['name']);
-move_uploaded_file($_FILES['arquivo']['tmp_name'], '/wamp64/www/agenda/imagens/' . $_POST['id']);
+move_uploaded_file($_FILES['arquivo']['tmp_name'], '../imagens/' . $_POST['id']);
 header("Location: index.php"); 
 ?>
